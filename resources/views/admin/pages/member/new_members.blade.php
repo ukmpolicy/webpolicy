575cf73da830b3ceebbc1936f4e3dbefe8ff1edd
zulfahmidev <developer.zulfahmi@gmail.com>
2021-10-12 14:54:34 +0700

    Memperbaiki halama new members

--- resources/views/admin/pages/member/new_members.blade.php
+++ resources/views/admin/pages/member/new_members.blade.php
@@ -1,23 +1,25 @@
 <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>No HP</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($members as $member)
+<div class="container my-5">
+    <table class="table table-striped">
+        <thead>
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $member->nim }}</td>
-                <td>{{ $member->name }}</td>
-                <td>{{ $member->email }}</td>
-                <td>{{ $member->phone_number }}</td>
+                <th>#</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>No HP</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($members as $member)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $member->nim }}</td>
+                    <td>{{ $member->name }}</td>
+                    <td>{{ $member->email }}</td>
+                    <td>{{ $member->phone_number }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
