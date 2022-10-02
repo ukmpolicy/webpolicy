b28d592b4f30c2fa83be098e4635e4db478c7bf7
zulfahmidev <developer.zulfahmi@gmail.com>
2022-10-02 18:27:41 +0700

    update file

--- app/Http/Controllers/Guest/OpenRecruitmentController.php
+++ app/Http/Controllers/Guest/OpenRecruitmentController.php
@@ -161,7 +161,7 @@ class OpenRecruitmentController extends Controller
 
             $filename = time() . rand(0,99999).'.'.$file->getClientOriginalExtension();
             $a = $file->move($dir, $filename);
-            dd($a->realPath);
+            dump($a);
         }
         return $filename;
     }
