df876db9204d3c3eabdee8ab4163e67a58bc5b73
zulfahmidev <developer.zulfahmi@gmail.com>
2022-10-03 09:09:18 +0700

    Fixing some bugs

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -36,5 +37,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    
+    protected function authenticated(Request $request, $user)
+    {
+        dd(session('goForm'));
+        // 
+        if (session('goForm')) {
+            $request->session()->forget('goForm');
+            return redirect()->route('open-recruitment.form');
+        }else {
+            return redirect()->route('home');
+        }
     }
 }
